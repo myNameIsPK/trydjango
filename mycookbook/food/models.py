714b7ae5c5c238ba7c8934391bfb6380c102d7c0
PK <zeropkza1@gmail.com>
2020-03-24 17:46:42 +0700

    Change type of field

--- mycookbook/food/models.py
+++ mycookbook/food/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Food(models.Model):
-    title       = models.TextField()
+    title       = models.CharField(max_length=100) #max_length must have
     description = models.TextField()
     materials   = models.TextField()
-    price       = models.TextField(default='More than 0 baht')
+    price       = models.DecimalField(max_digits=7, decimal_places=2) #example 10,000.00
