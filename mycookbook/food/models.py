dc75e8dfcd67f2ced0f35a47a4eb4551b247951f
PK <zeropkza1@gmail.com>
2020-03-24 18:07:45 +0700

    Add blank feater.

--- mycookbook/food/models.py
+++ mycookbook/food/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Food(models.Model):
     title       = models.CharField(max_length=100) #max_length must have
-    description = models.TextField()
-    materials   = models.TextField()
+    description = models.TextField(blank=True,null=True) #null = in DB can or cannot null
+    materials   = models.TextField(blank=False,null=False)
     price       = models.DecimalField(max_digits=7, decimal_places=2) #example 10,000.00
